6e9350b969db82900479487f9ae03d0fc1f2f24d
wayne <wayneliu0512@gmail.com>
2018-10-13 23:04:25 +0800

    online note pad

--- application.py
+++ application.py
@@ -1,15 +1,20 @@
-import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    headline = 'index'
-    now = datetime.datetime.now()
-    new_year = now.month == 1 and now.day == 1
-    # new_year = True
-    return render_template('hello.html', headline=headline, new_year=new_year)
+    if session["notes"] is None:
+        session["notes"] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+    return render_template('index.html', notes=session["notes"])
 
 @app.route('/bye')
 def bye():
